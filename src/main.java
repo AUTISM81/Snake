5d828c57b366769161c142e71825b886261ade1d
AUTISM81 <54669344+AUTISM81@users.noreply.github.com>
2020-04-09 10:35:59 +0200

    added Magnus files, Also made the square2 to have a constant update speed in movement so you cant stop it

--- src/main.java
+++ src/main.java
@@ -1,4 +1,12 @@
-public class main {
+/**
+ * This is a class
+ * Created 2020-03-25
+ *
+ * @author Magnus Silverdal
+ */
+public class Main {
     public static void main(String[] args) {
+        Graphics graphics = new Graphics(400,250,3);
+        graphics.start();
     }
 }
